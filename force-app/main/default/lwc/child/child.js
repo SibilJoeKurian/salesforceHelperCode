2726756573f120b353c9f52fd8f0fda6b0e2f82e
SIBIL JOE <sikurian@deloitte.ca>
2021-12-04 12:32:32 -0500

    removed unwanted lwc

--- force-app/main/default/lwc/child/child.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Child extends LightningElement {
-    handleEvent(){
-        const myCustomEvent=new CustomEvent('simple',{detail:'5'})
-        this.dispatchEvent(myCustomEvent)
-    }
-}
